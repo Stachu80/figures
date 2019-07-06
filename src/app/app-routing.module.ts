fbd1c70351ead5895301a0765b05b61fbe554fd1
Stachu80 <michal.stasinski@poczta.fm>
2019-07-06 15:21:00 +0200

    create data selectors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,29 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
-import {StoreModule} from '@ngrx/store';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
   {
-    path: 'welcome',
-    loadChildren: './welcome/welcome.module#WelcomeModule',
+    path: "welcome",
+    loadChildren: "./welcome/welcome.module#WelcomeModule"
   },
   {
-
-    path: 'selection',
-    loadChildren: './selection/selection.module#SelectionModule',
+    path: "selection",
+    loadChildren: "./selection/selection.module#SelectionModule"
   },
   {
-    path: '',
-    redirectTo: '/welcome',
-    pathMatch: 'full'
-  },
+    path: "",
+    redirectTo: "/welcome",
+    pathMatch: "full"
+  }
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules,
-  })],
+      preloadingStrategy: PreloadAllModules
+    })
+  ],
 
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { FormsModule } from '@angular/forms';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 @NgModule({
   declarations: [
     AppComponent
@@ -16,6 +18,9 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
